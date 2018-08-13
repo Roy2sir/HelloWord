69d7078a7b8fae59feb7c700c6730c2c9404f5d0
Roy2sir <37204338+Roy2sir@users.noreply.github.com>
2018-08-13 17:04:02 +0800

    Add files via upload

--- testjava/src/testjava/Test.java
+++ testjava/src/testjava/Test.java
@@ -1,7 +1,15 @@
 package testjava;
 
 public class Test {
+	public void function(int i) {
+		if(i>1) {
+			System.out.println("这是错误的");
+		}else {
+			System.out.println("这是正确的");
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println("hello word !!!");
+		System.out.println("第一次修改代码");
 	}
 }
